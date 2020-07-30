412a8c703c8da00f9d347b9d6fa1fb53d872b551
song-jia-lei <s13203867434@163.com>
2020-07-30 10:21:43 +0800

    text

---
+++ src/main/java/comm/sjl/HiWorld.java
@@ -0,0 +1,7 @@
+package comm.sjl;
+
+public class HiWorld {
+    public static void main(String[] args) {
+        System.out.println("hi world");
+    }
+}
---
+++ src/test/java/comm/sjl/AppTest.java
@@ -0,0 +1,20 @@
+package comm.sjl;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
